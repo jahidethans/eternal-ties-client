e84b3c6e24f157dc58c5ffe022ff6f7d1feaa587
Jahid Hasan <jahidjlp112233@gmail.com>
2023-11-25 01:40:47 +0600

    private route completed

--- src/PrivateRoute/PrivateRoute.jsx
+++ src/PrivateRoute/PrivateRoute.jsx
@@ -1,11 +1,25 @@
+import { useContext } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
+import { Navigate, useLocation } from "react-router-dom";
 
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation()
+  
 
-const PrivateRoute = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+ if(loading){
+    return <div className="flex items-center justify-center h-screen">
+        <div className="loading loading-infinity text-secondary w-[8rem]">Loading</div>
+      </div>}
+ 
+
+  if (!user?.email) {
+    return <Navigate state={location.pathname} to='/login'></Navigate>;
+  }
+
+  
+
+  return children;
 };
 
 export default PrivateRoute;
